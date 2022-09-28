1100f19af3fd6ab5de204533ff60ac29fbf3b8df
canon20paul <canon20paul@gmail.com>
2022-09-28 13:29:32 +0300

    Readme and Linter config

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-import './modules/addBook.js' ; // Module is evaluated!
-import  addBook  from './modules/addBook.js';
-import  displayBooks  from './modules/display.js';
-import selection  from './modules/navigation.js';
+import './modules/addBook.js';
+import addBook from './modules/addBook.js';
+import displayBooks from './modules/display.js';
+import selection from './modules/navigation.js';
+import { DateTime } from './modules/luxon.js';
 
-
-// const booksSection = document.getElementById('books-list');
 const timeDate = document.getElementById('date');
 const sectionOfBooks = document.querySelector('#books-section');
 const sectionContact = document.querySelector('#section-contact');
 const sectionForm = document.querySelector('#form-section');
-// const inputTitle = document.getElementById('title');
-// const inputAuthor = document.getElementById('author');
 const addBtn = document.getElementById('add-btn');
 const errorMesg = document.querySelector('.error-mesg');
 const links = document.querySelectorAll('.nav-links');
+// const removeBtn = document.querySelectorAll('.remove-btn');
 export const booksList = JSON.parse(localStorage.getItem('coward')) || [];
-console.log(sectionOfBooks);
-console.log(sectionContact);
-console.log(sectionForm);
-
+//Main Class
 export class Books {
     constructor(title, author) {
         this.title = title;
@@ -26,56 +21,33 @@ export class Books {
     }
 
     // displaybooks
-    static displayBookss() { Books.timeDisplay(); displayBooks()}
-    //     booksList.forEach((book, i) => {
-    //         booksSection.innerHTML += `<div class="book-detail" id="book-detail">
-    //   <div class="one-book">
-    //       <span class="book-title">"${book.title}"</span>
-    //       <span>by</span>
-    //       <span class="book-author">${book.author}</span>
-    //   </div>
-    //       <button type="button" onclick="Books.removeBook(${i})" class="remove-btn">Remove</button>
-    //   </div>`;
-    //     });
-    // }
-    // addbook
-    static addBooks() {       addBook();}
-        // const book = new Books(inputTitle.value, inputAuthor.value);
-        // if (inputTitle.value !== '' && inputAuthor.value !== '') {
-        //     booksList.push(book);
-        //     localStorage.setItem('coward', JSON.stringify(booksList));
-        //     errorMesg.classList.remove('active');
-        // } else {
-        //     errorMesg.classList.add('active');
-        // }
-    
-    // removebook
+    static displayBookss() { displayBooks(); }
 
-    removeBook(index) {
+    // addbook
+    static addBooks() { addBook(); location.reload(); }
+    // removebook
+    static removeBook(index) {
         booksList.splice(index, 1);
-        booksSection.innerHTML = '';
         localStorage.setItem('coward', JSON.stringify(booksList));
         Books.displayBookss();
+        location.reload();
     }
-
     //Display Date and Time
     static timeDisplay() {
-        let date = new Date();
-        let n = date.toDateString();
-        let time = date.toLocaleTimeString();
-        timeDate.innerText = n + '     ' + time;
+        const clock = DateTime.now();
+        timeDate.innerText = clock.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
     }
+    //end of Class Books
 }
 
 window.onload = Books.displayBookss();
-setInterval(Books.timeDisplay, 1000);
-addBtn.addEventListener('click', () => {
-    booksSection.innerHTML = '';
-    Books.addBook();
-    // inputTitle.value = '';
-    // inputAuthor.value = '';
-});
-
-// navigation
-
-links.forEach((link) => { link.addEventListener('click', e => selection(e.target.id));})
+const removeBtn = document.querySelectorAll('.remove-btn');
+setInterval(Books.timeDisplay, 1000); //Digital Time Display
+addBtn.addEventListener('click', () => Books.addBooks());//add book
+links.forEach((link) => { link.addEventListener('click', e => selection(e.target.id)); }) // Tabed navigation
+//Remove Book
+removeBtn.forEach((Btn) => {
+    Btn.addEventListener('click', (e) => {
+        Books.removeBook(e.target.id);
+    });
+})
--- modules/display.js
+++ modules/display.js
@@ -2,11 +2,11 @@
 import { booksList } from '/index.js'; // Module is evaluated!
 
 const booksSection = document.getElementById('books-list');
-  export default function displayBooks() {
-      booksSection.innerHTML = ''
-    // Books.timeDisplay()
-    booksList.forEach((book, i) => {
-        booksSection.innerHTML += `<div class="book-detail" id="book-detail">
+export default function displayBooks() {
+  booksSection.innerHTML = ''
+  // Books.timeDisplay()
+  booksList.forEach((book, i) => {
+    booksSection.innerHTML += `<div class="book-detail" id="book-detail">
       <div class="one-book">
           <span class="book-title">"${book.title}"</span>
           <span>by</span>
@@ -14,5 +14,5 @@ const booksSection = document.getElementById('books-list');
       </div>
           <button type="button" class="remove-btn" id="${i}">Remove</button>
       </div>`;
-     });   
+  });
 }
